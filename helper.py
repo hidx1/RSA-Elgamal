cf92f5f5e3a8cc0e8edc4739ed009c2d051518d8
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2020-10-28 01:25:19 +0700

    Add diffie helman

--- helper.py
+++ helper.py
@@ -5,6 +5,7 @@ def egcd(a, b):
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
+
 def modinv(a, m):
     g, x, y = egcd(a, m)
     if g != 1:
@@ -12,11 +13,16 @@ def modinv(a, m):
     else:
         return x % m
 
+
 def writeToFile(data, mode):
     if (mode == 0):
         f = open("public.pub", "w")
     else:
         f = open("private.pri", "w")
-    
+
     f.write(data)
     f.close()
+
+
+def diffie_helman(n, g, x, y):
+    return pow((pow(g, x) % n), (pow(g, y) % n)) % n
