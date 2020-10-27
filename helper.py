b186bb9e61470fab96a80a55afae60a5cd7e7343
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2020-10-28 02:13:12 +0700

    Add el gamal func

--- helper.py
+++ helper.py
@@ -13,6 +13,7 @@ def modinv(a, m):
     else:
         return x % m
 
+
 def writeKeyToFile(data, mode):
     if (mode == 0):
         f = open("public.pub", "w")
@@ -22,12 +23,13 @@ def writeKeyToFile(data, mode):
     f.write(data)
     f.close()
 
+
 def writeCodeToFile(codedText):
     f = open("cipherText.txt", "w")
     result = ""
-    for i in range(int(len(codedText)/3)):
+    for i in range(int(len(codedText) / 3)):
         print(f"i: {i}")
-        block = codedText[i*3:i*3+3]
+        block = codedText[i * 3:i * 3 + 3]
         print(f"block: {block}")
         char = chr(int(block))
         result += char
@@ -35,30 +37,44 @@ def writeCodeToFile(codedText):
     f.write(char)
     f.close()
 
+
 def codeMessage(msg):
     result = ""
     for char in msg:
         code = str(ord(char))
-        headingLength = 3-len(code)
+        headingLength = 3 - len(code)
         for i in range(headingLength):
-            code = '0'+code
+            code = '0' + code
         result += code
     if (len(result) % 6 != 0):
         result += "256"
     return result
 
+
 def diffie_helman(n, g, x, y):
-    return pow((pow(g, x) % n), (pow(g, y) % n)) % n
+    return pow(g, x * y, n)
+
+
+def el_gamal_encrypt():
+    return
+
+
+def el_gamal_decrypt():
+    return
+
 
 def gcd(p, q):
     while q != 0:
-        p, q = q, p%q
+        p, q = q, p % q
     return p
+
 
 def is_coprime(x, y):
     return gcd(x, y) == 1
 
+
 if __name__ == "__main__":
     for i in range(1000000000000000000000000000000000000000000000000000000000000000000000000000):
-        if (is_coprime(i, 1210145036063417205622974722189679300425594239971785447214628764089958055217275328377339625440400256608307406736692769091333622175530734487728425224810942804559270668428759323294557805268314468224293367130593570261803241958640659109949848064843598016986040681231330709222324227117155849109075659061411541132721)):
+        if (is_coprime(i,
+                       1210145036063417205622974722189679300425594239971785447214628764089958055217275328377339625440400256608307406736692769091333622175530734487728425224810942804559270668428759323294557805268314468224293367130593570261803241958640659109949848064843598016986040681231330709222324227117155849109075659061411541132721)):
             print(i)
